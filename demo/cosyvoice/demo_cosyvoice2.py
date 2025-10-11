c3baae4ab7a33a4629a98a18b34bb71ff2a97fcb
boomytc <767478228@qq.com>
2025-10-11 14:01:04 +0800

    refactor(demo): 优化推理调用，提取文本变量并重构结果保存逻辑

--- demo/cosyvoice/demo_cosyvoice2.py
+++ demo/cosyvoice/demo_cosyvoice2.py
@@ -16,15 +16,29 @@ os.makedirs('outputs', exist_ok=True)
 cosyvoice = CosyVoice2('models/CosyVoice2-0.5B', load_jit=False, load_trt=False, fp16=False)
 
 prompt_speech_16k = load_wav('./asset/zero_shot_prompt.wav', 16000)
+prompt_text = '希望你以后能够做的比我还好呦。'
+
+text1 = '收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。'
+text2 = '在他讲述那个荒诞故事的过程中，他突然[laughter]停下来，因为他自己也被逗笑了[laughter]。'
 
 # zero_shot usage
-for i, j in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False, speed=1.0, text_frontend=True)):
-    torchaudio.save('outputs/zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+result_zero_shot = cosyvoice.inference_zero_shot(text1, prompt_text, prompt_speech_16k, stream=False, speed=1.0, text_frontend=True)
 
-# 精细控制，支持的控制标记请查看 cosyvoice/tokenizer/tokenizer.py#L248
-for i, j in enumerate(cosyvoice.inference_cross_lingual('在他讲述那个荒诞故事的过程中，他突然[laughter]停下来，因为他自己也被逗笑了[laughter]。', prompt_speech_16k, stream=False, speed=1.0, text_frontend=True)):
-    torchaudio.save('outputs/fine_grained_control_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+# fine_grained_control usage
+result_fine_grained_control = cosyvoice.inference_cross_lingual(text2, prompt_speech_16k, stream=False, speed=1.0, text_frontend=True)
 
 # instruct usage
-for i, j in enumerate(cosyvoice.inference_instruct2('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', '用四川话说这句话', prompt_speech_16k, stream=False, speed=1.0, text_frontend=True)):
-    torchaudio.save('outputs/instruct_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+result_instruct = cosyvoice.inference_instruct2(text1, '用四川话说这句话', prompt_speech_16k, stream=False, speed=1.0, text_frontend=True)
+
+result_list = [
+    ('zero_shot', result_zero_shot),
+    ('fine_grained_control', result_fine_grained_control),
+    ('instruct', result_instruct)
+]
+
+for label, result in result_list:
+    for i, j in enumerate(result):
+        if (i > 0):
+            torchaudio.save('outputs/{}_{}.wav'.format(label, i), j['tts_speech'], cosyvoice.sample_rate)
+        else:
+            torchaudio.save('outputs/{}.wav'.format(label), j['tts_speech'], cosyvoice.sample_rate)
