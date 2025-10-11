1108c89bd1ab18d0932e8b8f8dfd71cdaa5b6760
boomytc <767478228@qq.com>
2025-10-11 09:08:35 +0800

    refactor(demo): 修改模型缓存和输出目录路径
    将模型缓存目录从绝对路径改为相对路径"models/kitten-tts-nano-0.1"
    将输出目录从"output"改为"outputs"

--- demo/kittentts/demo_kittentts.py
+++ demo/kittentts/demo_kittentts.py
@@ -1,7 +1,7 @@
 import os
 
 # 设置模型下载路径 - 必须在导入任何huggingface相关库之前设置
-model_cache_dir = "/Users/boom/Model/TTS/kitten-tts-nano-0.1"
+model_cache_dir = "models/kitten-tts-nano-0.1"
 os.makedirs(model_cache_dir, exist_ok=True)
 os.environ["HF_HOME"] = model_cache_dir
 os.environ["HUGGINGFACE_HUB_CACHE"] = model_cache_dir
@@ -18,5 +18,5 @@ audio = model.generate("fuck! what are you talking about?", voice='expr-voice-2-
 # available_voices = [  'expr-voice-2-m', 'expr-voice-2-f', 'expr-voice-3-m', 'expr-voice-3-f',  'expr-voice-4-m', 'expr-voice-4-f', 'expr-voice-5-m', 'expr-voice-5-f' ]
 
 # 保存音频
-os.makedirs('output', exist_ok=True)
-sf.write('output/output.wav', audio, 24000)
+os.makedirs('outputs', exist_ok=True)
+sf.write('outputs/output.wav', audio, 24000)
